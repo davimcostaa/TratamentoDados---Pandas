8a3736c1080bb7096520d6e7b4f48b5fc4c0c441
Davi Costa <92067624+davimcostaa@users.noreply.github.com>
2022-10-24 09:03:58 -0300

    :art: [UPDATE] Concertando informações do script.

--- siafi.py
+++ siafi.py
@@ -7,7 +7,7 @@ Created on Tue Jul  5 10:57:53 2022
 
 import pandas as pd
 
-siafi = pd.read_excel("Z:/CGETNO/CGETNO/Estágio/Davi/siafi.xlsx", sheet_name = 'CGETNO', header=9) 
+## siafi = pd.read_excel() 
 print(siafi)
 siafi = siafi.drop(siafi.index[0:8]) #apaga as linhas do cabeçalho
 print(siafi)
@@ -18,7 +18,7 @@ print(siafi)
 
 siafi = siafi.dropna(thresh=12)
 
-lista_cr = pd.read_excel("Z:/CGETNO/CGETNO/Estágio/Davi/listas_formulário 1.xlsx", sheet_name = 'Lista_CR')
+# lista_cr = pd.read_excel()
 
 lista_cr['UG']=lista_cr['UG'].astype(int) #troca o tipo da coluna para int
 siafi['UG'] = siafi['UG'].astype(int) #troca o tipo da coluna para int
@@ -31,25 +31,3 @@ siafi['UG Descrição'] = siafi['CR'] # coloca os nomes corretos das UG
 siafi.drop(['CR'], axis=1, inplace=True) #apaga a coluna CR para não repetir os nomes.
 print(siafi)
 
-
-import psycopg2
-conect2 = psycopg2.connect(host='localhost', database='siafi',
-user='postgres', password='1234')
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:1234@localhost:5432/siafi')
-
-
-siafi.to_sql('siafi', engine, schema='teste_siafi')
-
-print(siafi)
-
-file_name = 'siafi_corrigido3.xlsx' # determinando nome do arquivo
-  
-siafi.to_excel(file_name) #nome do arquivo
-# print('DataFrame convertido em Excel com sucesso.')
-
-import os
-excel = pd.ExcelWriter('Z:/CGETNO/CGETNO/Estágio/Davi/siaficorrigido3.xlsx', engine='xlsxwriter')
-siafi.to_excel(excel)
-excel.save()
